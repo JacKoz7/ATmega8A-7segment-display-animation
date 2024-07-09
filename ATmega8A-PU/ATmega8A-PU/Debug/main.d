169b91c594d437091493205bcbb8c0f02edb6c1f
Jacek <jacuskozlowski@gmail.com>
2024-07-09 17:38:53 +0200

    commit

--- ATmega8A-PU/ATmega8A-PU/Debug/main.d
+++ ATmega8A-PU/ATmega8A-PU/Debug/main.d
@@ -12,7 +12,9 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -41,3 +43,7 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
--- ATmega8A-PU/ATmega8A-PU/main.c
+++ ATmega8A-PU/ATmega8A-PU/main.c
@@ -1,32 +1,53 @@
 #define F_CPU 1000000UL
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/pgmspace.h>
+
+// Define 7-segment display patterns for "OPEN" and "CLOSED" in program memory
+const uint8_t PROGMEM segments[][7] = {
+	{0b00111111, 0b01110011, 0b01111001, 0b00110111, 0}, // OPEN
+	{0b00111001, 0b00111000, 0b00111111, 0b01101101, 0b01111001, 0b01011110, 0} // CLOSED
+};
 
 int main(void) {
-	// dp g f e d c b a
-	//   a
-	// f   b
-	//   g
-	// e   c
-	//   d   dp
-	const uint8_t jh[] = {0b01110111, 0b00110111, 0b00000110, 0b01110111, 0b00000000};
-	DDRD = 0xFF; // ca³y port D na wyjœcie (segmenty)
-	DDRC = 0b00001111; // pierwsza po³owa portu C na wyjœcie (modu³y)
+	// Configure I/O ports
+	DDRD = 0xFF;  // Set PORTD as output for segments
+	DDRC |= 0x0F; // Set lower 4 bits of PORTC as output for digit selection
+	DDRB &= ~(1 << PB0); // Set PB0 as input for button
+	PORTB |= (1 << PB0); // Enable pull-up resistor for PB0
 
-	int i = 0;
+	uint8_t i = 0, state = 0, last_button = 0, button_pressed = 0;
 	
 	while (1) {
-		// Wyœwietlanie cyfr 
-		for (int k = 0; k < 65; k++) { // Powtórz przez 65ms
-			for (int j = 0; j < 4; j++) {
-				PORTD = 0x00; // Turn off all segments
-				PORTC = ~(1 << j); // wybór kolumny (cyfry)
-				PORTD = jh[(i + j) % 5]; // wyœwietlenie odpowiedniej cyfry
+		// Read button state (active low)
+		uint8_t button = !(PINB & (1 << PB0));
+		
+		// Button press detection with debouncing
+		if (button && !last_button) {
+			_delay_ms(10); // Debounce 
+			button_pressed = 1;
+			} else if (!button && last_button) {
+			_delay_ms(10); // Debounce delay for button release
+			if (button_pressed) {
+				state ^= 1; // Toggle state between OPEN and CLOSED
+				i = 0;      // Reset display index
+				button_pressed = 0;
+			}
+		}
+		last_button = button;
+
+		// Display loop
+		for (uint8_t k = 0; k < 50; k++) {
+			for (uint8_t j = 0; j < 4; j++) {
+				PORTD = 0x00; // Turn off all segments 
+				PORTC = ~(1 << j); // Select digit (active low)
+				// Display appropriate segment pattern
+				PORTD = pgm_read_byte(&segments[state][(i + j) % (state ? 0x7 : 0x5)]);
 				_delay_ms(1);
 			}
 		}
-
-		// Przesuniêcie cyfr
-		i = (i + 1) % 5; // przesuñ indeks, aby przejœæ do nastêpnego zestawu cyfr
+		
+		// Move to next character in the sequence
+		i = (i + 1) % (state ? 0x7 : 0x5);
 	}
 }
